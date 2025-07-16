bacc72fc2049df7fe9cc1709648b2acf8e3aee24
jamesnulliu <jamesnulliu@gmail.com>
2025-07-16 02:56:18 +0000

    Initial Update

---
+++ include/leetcode/math/vec_add.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace leetcode
+{
+void launch_vec_add(const float* a, const float* b, float* c, int n);
+}  // namespace leetcode
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <array>
+#include <iostream>
+#include <print>
+
+#include <leetcode/math/vec_add.hpp>
+
+auto main() -> int
+{
+    std::println("LeetCode - Math - Vector Addition");
+
+    return 0;
+}
